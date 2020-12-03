3c5296d24cb27b77085d28da4cd90dcdcc0b1477
Taowen <1325772089@qq.com>
2020-12-03 23:21:15 +0800

    更新名称

--- screenshot_to_media.php
+++ screenshot_to_media.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: copy image
-Plugin URI: http://wordpress.org/plugins/copy-image/
-Description: 复制剪贴板图片到媒体库
+Plugin Name: screenshot to media
+Plugin URI: http://wordpress.org/plugins/screenshot-to-media/
+Description: Paste the screenshot to the media library（将截图粘贴到媒体库）
 Author: iwowen
 Version: 1.7.2
 Author URI: https://www.iwowen.cn/
