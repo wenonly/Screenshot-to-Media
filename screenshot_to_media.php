506bd2a686b4dc33350b34cf12e47e61d9c26819
iwowen <1325772089@qq.com>
2020-12-05 16:26:10 +0800

    更函数新命名空间，校验函数是否存在

--- screenshot_to_media.php
+++ screenshot_to_media.php
@@ -9,62 +9,69 @@
 * License: GPLv3 or later
 * License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
-
-function iwowen_is_media() {
-    return in_array($GLOBALS['pagenow'], array('media-new.php', 'upload.php', 'post-new.php', 'post.php'));
+if ( !function_exists( 'screenshot_to_media_is_media' ) ) { 
+    function screenshot_to_media_is_media() {
+        return in_array($GLOBALS['pagenow'], array('media-new.php', 'upload.php', 'post-new.php', 'post.php'));
+    }
 }
 
-function iwowen_paste_image_to_media() {
-    if (iwowen_is_media()) {
-    ?>
-        <script type="text/javascript">
-        document.addEventListener('paste', function (event) {
-            var items = event.clipboardData && event.clipboardData.items;
-            var file = null;
-            if (items && items.length) {
-                // 检索剪切板items
-                for (var i = 0; i < items.length; i++) {
-                    if (items[i].type.indexOf('image') !== -1) {
-                        file = items[i].getAsFile();
-                        break;
+
+if ( !function_exists( 'screenshot_to_media_paste_image_to_media' ) ) {
+    function screenshot_to_media_paste_image_to_media() {
+        if (function_exists('screenshot_to_media_is_media') && screenshot_to_media_is_media()) {
+        ?>
+            <script type="text/javascript">
+            document.addEventListener('paste', function (event) {
+                var items = event.clipboardData && event.clipboardData.items;
+                var file = null;
+                if (items && items.length) {
+                    // 检索剪切板items
+                    for (var i = 0; i < items.length; i++) {
+                        if (items[i].type.indexOf('image') !== -1) {
+                            file = items[i].getAsFile();
+                            break;
+                        }
                     }
                 }
-            }
-            if (!file) return
-            let successed = false
-            try {
-                // 文章编辑页面
-                // 如果窗口是打开状态
-                if (wp.media.frame.modal.$el.is(':visible')) {
-                    wp.media.frame.uploader.uploader.uploader.addFile(file)
+                if (!file) return
+                let successed = false
+                try {
+                    // 文章编辑页面
+                    // 如果窗口是打开状态
+                    if (wp.media.frame.modal.$el.is(':visible')) {
+                        wp.media.frame.uploader.uploader.uploader.addFile(file)
+                        successed = true
+                    }
+                } catch (error) {}
+                try {
+                    // 媒体新增页面
+                    uploader.addFile(file)
                     successed = true
-                }
-            } catch (error) {}
-            try {
-                // 媒体新增页面
-                uploader.addFile(file)
-                successed = true
-            } catch (error) {}
-            try {
-                // 媒体列表页面
-                wp.media.frames.browse.uploader.uploader.uploader.addFile(file)
-                successed = true
-            } catch (error) {}
-            // 执行成功，清空剪贴板
-            if (successed) {
-                function handler(e) {
-                    e.clipboardData.setData('text/plain', '');
-                    e.preventDefault();
-                }
-                document.addEventListener('copy', handler);   // 增加copy监听
-                document.execCommand('copy');   // 执行copy命令触发监听
-                document.removeEventListener('copy', handler);   // 移除copy监听，不产生影响
-            }   
-        });
-        </script>
-    <?php
-    }
-};
+                } catch (error) {}
+                try {
+                    // 媒体列表页面
+                    wp.media.frames.browse.uploader.uploader.uploader.addFile(file)
+                    successed = true
+                } catch (error) {}
+                // 执行成功，清空剪贴板
+                if (successed) {
+                    function handler(e) {
+                        e.clipboardData.setData('text/plain', '');
+                        e.preventDefault();
+                    }
+                    document.addEventListener('copy', handler);   // 增加copy监听
+                    document.execCommand('copy');   // 执行copy命令触发监听
+                    document.removeEventListener('copy', handler);   // 移除copy监听，不产生影响
+                }   
+            });
+            </script>
+        <?php
+        }
+    };
+    
+    add_action('admin_footer','screenshot_to_media_paste_image_to_media');
+}
 
-add_action('admin_footer','iwowen_paste_image_to_media');
+
+
 
